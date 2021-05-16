724f41c847be33882ff84b45fab39b34875ee30e
Ashfiii <alam19231@gmail.com>
2021-05-16 20:25:48 +0530

    Updates

---
+++ precache-manifest.a439a34bcc383d5d12ce344877f63f67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9e4f3c96ac8af6bb8ced3b50df68fa",
+    "url": "/recipee-app/index.html"
+  },
+  {
+    "revision": "9068db9929f077679ad5",
+    "url": "/recipee-app/static/css/main.9906f2ec.chunk.css"
+  },
+  {
+    "revision": "e33e33a1e6df0953dab2",
+    "url": "/recipee-app/static/js/2.29aeef11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipee-app/static/js/2.29aeef11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9068db9929f077679ad5",
+    "url": "/recipee-app/static/js/main.2969c4ae.chunk.js"
+  },
+  {
+    "revision": "86eb23115fbde348f19d",
+    "url": "/recipee-app/static/js/runtime-main.d730021c.js"
+  }
+]);
